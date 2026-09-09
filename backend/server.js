1c0d9abeef58a2f1fa447ff3735d56160fedf17e
Joyeeta Das <joyeetadas597@gmail.com>
2026-09-09 18:43:47 +0530

    socket.io in backend/server side done

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,19 @@
 import "dotenv/config";
 import app from "./src/app.js";
 import connectDatabase from "./src/config/database.js";
+import http from "http"
+import { initSocket } from "./src/sockets/server.socket.js";
 
 const port = Number(process.env.PORT) || 5000;
 
 const startServer = async () => {
+	const httpServer=http.createServer(app)
+	initSocket(httpServer);
+	
 	try {
 		await connectDatabase();
 
-		app.listen(port, () => {
+		httpServer.listen(port, () => {
 			console.log(`Server is running on port ${port}`);
 		});
 	} catch (error) {
