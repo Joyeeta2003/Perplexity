4f5555194626d9678f820418231e590c775a7d6b
Joyeeta Das <joyeetadas597@gmail.com>
2026-09-09 09:06:48 +0530

    state layer done

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './app/index.css'
 import App from './app/App.jsx'
+import { store } from './app/app.store.js'
+import {Provider} from 'react-redux'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <provider store = {store}>
+     <App />
+  </provider>
 )
