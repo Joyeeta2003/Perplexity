00a8667b4f9eed06ce9769a4e6080d80d4c4e926
Joyeeta Das <joyeetadas597@gmail.com>
2026-09-10 13:03:31 +0530

    generate chat title done

--- backend/src/controllers/chat.controller.js
+++ backend/src/controllers/chat.controller.js
@@ -1,11 +1,27 @@
-import { generateRespose } from "../services/ai.services.js";
+import { generateRespose, generateChatTitle } from "../services/ai.services.js";
+import chatModel from '../models/chat.model.js'
+import messageModel from '../models/message.model.js'
 
 export async function sendMessage(req,res) {
     const {message} = req.body
 
+    const title = await generateChatTitle(message);
     const result = await generateRespose(message);
 
-    res.json({
-        aiMessage:result
+    const chat = await chatModel.create({
+        user:req.user.id,
+        title
+    })
+
+    const aiMessage = await messageModel.create({
+        chat:chat._id,
+        content:result,
+        role:"AI"
+    })
+
+    res.status(201).json({
+        title,
+        chat,
+        aiMessage
     })
 }
--- backend/src/services/ai.services.js
+++ backend/src/services/ai.services.js
@@ -1,14 +1,35 @@
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
-import {HumanMessage} from "@langchain/core/messages";
+import {HumanMessage, SystemMessage} from "langchain";
+import {ChatMistralAI} from '@langchain/mistralai'
 
-const model = new ChatGoogleGenerativeAI({
+
+const GeminiModel = new ChatGoogleGenerativeAI({
   model: "gemini-3.7-flash",
   apiKey: process.env.GEMINI_API_KEY
 });
 
+const MistralModel = new ChatMistralAI({
+  model:"mistral-small-latest",
+  apiKey:process.env.MISTRAL_API_KEY
+})
+
 export async function generateRespose(message) {
-  const response = await model.invoke([
+  const response = await GeminiModel.invoke([
     new HumanMessage(message)
   ])
+  return response.text
+}
+
+export async function generateChatTitle(message) {
+  const response = await MistralModel.invoke([
+    new SystemMessage(`you are a helpful assistant that genarate concise and descriptive title for the chat conversation.
+      user will provide you with the first message of a chat conversation, and you will generate a title that capture the essence os the conversation in 2-4 words.
+      the title should be clear and relevant and engaging, giving user a quick understanding of the chat's topic.`
+    ),
+    new HumanMessage(
+      `Genarate a title for the chat conversation based on the following first message :"${message}" `
+    )
+  ])
+
   return response.text
 }
